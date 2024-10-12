bf5972aed5e32b0d8e3077b08c1396dead51d001
htmcharles <hatumacharles1@gmail.com>
2024-10-12 20:49:46 +0200

    sign up

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:conn/pages/auth/signup_page.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Center vertically
           children: [
-            Text('Login Here'),
-            Text('Welcome back you have been missed!'),
-            TextInput(text: 'Email'),
-            TextInput(text: 'Password'),
-            Text('Forgot password'),
-            
+            const Text('Login Here'),
+            const Text('Welcome back, you have been missed!'),
+            const TextInput(text: 'Email'),
+            const TextInput(text: 'Password'),
+            const Text('Forgot password?'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const SignupPage(),
+                  ),
+                );
+              },
+              child: const Text('Login'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const SignupPage(),
+                  ),
+                );
+              },
+              child: const Text('Create Account'),
+            ),
+            const Text('Or continue with'),
+            Image.asset('images/google.png'),
+            Image.asset('images/facebook.png'),
+            Image.asset('images/apple.png'),
           ],
         ),
       ),
     );
   }
 }
+
 class TextInput extends StatelessWidget {
   final String text;
 
@@ -27,12 +53,18 @@ class TextInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: text, // For placeholder
-      ),
-      style: const TextStyle(
-        backgroundColor: Color(0xFFF1F4FF), // Use correct color definition
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0), // Add padding
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: text, // Placeholder
+          filled: true,
+          fillColor: const Color(0xFFF1F4FF), // Use correct background color in decoration
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: BorderSide.none,
+          ),
+        ),
       ),
     );
   }
