e130ebdf112e7d107f07215e1a369576815cc83d
htmcharles <hatumacharles1@gmail.com>
2024-10-13 20:58:02 +0200

    loginpage

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -6,6 +6,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center, // Center vertically
